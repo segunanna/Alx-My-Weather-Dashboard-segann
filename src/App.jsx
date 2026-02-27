73510c0150dfa351fa67c01bf07a48dbfb84f39e
segunanna <annasegun8@gmail.com>
2026-02-27 14:43:14 +0100

    Made some changes in my navebad, about and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
+import FooterPage from './components/FooterPage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+    <div className="flex flex-col min-h-screen">
+    
+      <Navbar />
+
+      {/* Main content */}
+        <main className="flex-grow bg-gray-100">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+     
+      <FooterPage />
+    </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
