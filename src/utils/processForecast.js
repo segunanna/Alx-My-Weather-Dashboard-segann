8f7d413aa0021163c92f9c3db311f70e69ce075d
segunanna <annasegun8@gmail.com>
2026-02-27 14:37:07 +0100

    Initial commit: Process forecast

--- src/utils/processForecast.js
+++ src/utils/processForecast.js
@@ -1,1 +1,18 @@
+export function processForecastData(list) {
 
+  const daily = {};
+
+  list.forEach((item) => {
+    const date = item.dt_txt.split(" ")[0];
+    if (!daily[date] && item.dt_txt.includes("12:00:00")) {
+      daily[date] = {
+        date,
+        temp: Math.round(item.main.temp),
+        description: item.weather[0].description,
+        icon: item.weather[0].icon,
+      };
+    }
+  });
+
+  return Object.values(daily);
+}
